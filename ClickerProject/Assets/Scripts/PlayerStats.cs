ac2112aac5c715191ddf0b31984c805549230250
Jack Tugby <masterjackt@hotmail.co.uk>
2017-08-19 02:24:42 +0100

    Updated Project a lot

---
+++ ClickerProject/Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    public static int
+        coins = 0,
+        currentXP = 0,
+        currentLevel = 1,
+        currentDamage = 10;
+
+    public static float
+        damageMultiplier = 1;
+
+    public void CoinClick()
+    {
+        if(coins < 999999)
+            coins++;
+    }
+}
